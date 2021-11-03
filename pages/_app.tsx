e80e7b882690971d36a1a67bc8fc8a60e0380d6b
Lemit <Mihtrop2005@yandex.ru>
2021-11-03 23:24:26 +0300

    ym

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import { AppProps } from 'next/app'
 import '../styles/index.css'
-
+import withYM from "next-ym";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
