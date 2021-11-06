eee9e03c374d3788949cad4603156af14a5de5bc
Lemit <Mihtrop2005@yandex.ru>
2021-11-06 19:50:04 +0300

    first upd

--- src/util/const.js
+++ src/util/const.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = "http://shrouded-retreat-21347.herokuapp.com";
+export const API_BASE_URL = "https://shrouded-retreat-21347.herokuapp.com";
 
 export const ACCESS_TOKEN = "accessToken";
 
