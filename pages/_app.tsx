e773e14cfb483e5fc3da1e409fa391aafd64b106
Lemit <Mihtrop2005@yandex.ru>
2021-11-06 17:03:16 +0300

    fdholdsf

--- pages/_app.tsx
+++
@@ -1,7 +0,0 @@
-import { AppProps } from 'next/app'
-import '../styles/index.css'
-import withYM from "next-ym";
-
-export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+    return(
+    <>
+        <h1>Home Works</h1>
+        <p>Hello</p>
+    </>
+    )
+}
+
+export default Home;
