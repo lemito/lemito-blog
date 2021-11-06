8eb722c0092b65439ca9ab2d5a42c6f021bd69f5
Lemit <Mihtrop2005@yandex.ru>
2021-11-06 21:04:13 +0300

    profiling

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -288,6 +288,8 @@ module.exports = function(webpackEnv) {
         .filter(ext => useTypeScript || !ext.includes('ts')),
       alias: {
         // Support React Native Web
+        // Добавил поддержку профилирования
+        'react-dom$': 'react-dom/profiling',
         // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
         'react-native': 'react-native-web',
         // Allows for better profiling with ReactDevTools
