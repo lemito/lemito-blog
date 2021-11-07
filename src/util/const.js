b39f1f7a0fc753b49ce0c8c5a1ce41111fa80956
Lemit <Mihtrop2005@yandex.ru>
2021-11-07 14:30:31 +0300

    access

--- src/util/const.js
+++ src/util/const.js
@@ -5,9 +5,7 @@ for (i=1;i<8;i++) {
     var c = Math.floor(Math.random()*chars.length + 1);
     passwd += chars.charAt(c)
 }
-
 export const API_BASE_URL = "https://shrouded-retreat-21347.herokuapp.com";
-
 export const ACCESS_TOKEN = passwd;
-
 export const LETTERS_CONTENT_MODE = ["ALL", "PUBLIC"];
+console.log("Your accedd token is "+ passwd)
