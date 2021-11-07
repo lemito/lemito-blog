84e9492423bed0eaba6b58b59307d8200422594a
Lemit <Mihtrop2005@yandex.ru>
2021-11-07 14:26:11 +0300

    accedd

--- src/util/const.js
+++ src/util/const.js
@@ -1,5 +1,16 @@
+var i
+var passwd = '';
+var chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-__-';
+for (i=1;i<8;i++) {
+    var c = Math.floor(Math.random()*chars.length + 1);
+    passwd += chars.charAt(c)
+}
+
 export const API_BASE_URL = "https://shrouded-retreat-21347.herokuapp.com";
 
-export const ACCESS_TOKEN = "accessToken";
+export const ACCESS_TOKEN = passwd;
 
 export const LETTERS_CONTENT_MODE = ["ALL", "PUBLIC"];
+
+console.log(ACCESS_TOKEN)
+
