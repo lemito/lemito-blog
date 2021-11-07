af8b532e308a9dc6310cf92ea6cd9d494baefcd7
Lemit <Mihtrop2005@yandex.ru>
2021-11-07 14:28:03 +0300

    dfsfsf

--- src/util/const.js
+++ src/util/const.js
@@ -11,6 +11,3 @@ export const API_BASE_URL = "https://shrouded-retreat-21347.herokuapp.com";
 export const ACCESS_TOKEN = passwd;
 
 export const LETTERS_CONTENT_MODE = ["ALL", "PUBLIC"];
-
-console.log(ACCESS_TOKEN)
-
