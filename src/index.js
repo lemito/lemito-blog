51f38cc614b8ef77257ed91a2c64a87c1efdd27f
Lemit <Mihtrop2005@yandex.ru>
2021-11-08 17:46:21 +0300

    tostr

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ import './index.css';
 import App from './components/app/App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.renderToString(<App />, document.getElementById('root'));
 
 serviceWorker.unregister();
