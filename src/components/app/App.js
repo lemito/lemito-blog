7f8e41884e07822c9a9efeab4688fc8736dc76a3
Lemit <Mihtrop2005@yandex.ru>
2021-11-08 17:38:09 +0300

    Удалил ненужные строки

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
-import CookieConsent, { Cookies } from "react-cookie-consent";
 import PrivateRoute from "../private-route/PrivateRoute";
 import Login from "../login/Login";
 import Authorship from "../authorship/authorship/Authorship";
