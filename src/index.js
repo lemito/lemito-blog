af9179b6d00c4904931836791e77221d0056fcb6
Lemit <Mihtrop2005@yandex.ru>
2021-11-08 18:04:25 +0300

    downgrade1

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
-//import ReactDOM from 'react-dom';
-import ReactDOMServer from 'react-dom/server';
+import ReactDOM from 'react-dom';
+//import ReactDOMServer from 'react-dom/server';
 
 import './index.css';
 import App from './components/app/App';
 import * as serviceWorker from './serviceWorker';
 
-//ReactDOM.render(<App />, document.getElementById('root'));
-ReactDOMServer.renderToString(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
+//ReactDOMServer.renderToString(<App />, document.getElementById('root'));
 
 serviceWorker.unregister();
