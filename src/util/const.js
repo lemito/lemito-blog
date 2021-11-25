c981057767a1aca085f9ee54c049fc8a1e157532
Lemit <Mihtrop2005@yandex.ru>
2021-11-25 16:02:54 +0300

    closure

--- src/util/const.js
+++ src/util/const.js
@@ -1,14 +1,8 @@
 console.clear()
-var i
-var passwd = '';
-var chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-__-';
-for (i=1;i<8;i++) {
-    var c = Math.floor(Math.random()*chars.length + 1);
-    passwd += chars.charAt(c)
-}
+var a,b="";for(a=1;8>a;a++)b+="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-__-".charAt(Math.floor(66*Math.random()+1));console.log("Your access token is "+b);console.log("Now your lang is "+(navigator.language||navigator.g));
 export const API_BASE_URL = "https://shrouded-retreat-21347.herokuapp.com";
-export const ACCESS_TOKEN = passwd;
+export const ACCESS_TOKEN = b;
 export const LETTERS_CONTENT_MODE = ["ALL", "PUBLIC"];
-console.log("Your access token is "+ passwd)
+/*console.log("Your access token is "+ passwd)
 const language = navigator.language || navigator.userLanguage || 'en'; 
-console.log("Now your lang is "+ language)
+console.log("Now your lang is "+ language)*/
