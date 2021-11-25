fa22851efa5f842f5c2f6b1533c0c32ec344c732
Lemit <Mihtrop2005@yandex.ru>
2021-11-25 17:12:15 +0300

    Upd local 2

--- src/components/left-panel/footer/Footer.js
+++ src/components/left-panel/footer/Footer.js
@@ -1,28 +1,24 @@
 import React, { Component } from "react";
-
 import "./Footer.css";
+import IsAb from './IsAb';
+import NotAb from './NotAb';
 
 class Footer extends Component {
-
- 
-
   render() {
-
     const { isAbout } = this.props;
-
     return(
       <div className="footer-div">
         <span onClick={ this.props.showAbout }>
-          { isAbout ? footerTitles[1].title : footerTitles[0].title } 
+          { isAbout ? <NotAb /> : <IsAb /> } 
         </span>
       </div>
     );
   }
-}
+};
 
-const footerTitles = [
+/*const footerTitles = [
   {value: "open", title: "обо мне"},
   {value: "close", title: "закрыть"}
-];
+];*/
 
 export default Footer;
---
+++ src/components/left-panel/footer/NotAb.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import {FormattedMessage } from 'react-intl';
+
+class NotAb extends Component {
+  render() {
+    return (
+      <FormattedMessage id="b-close" defaultMessage="Закрыть" description="закрыть" />
+    );
+  }
+};
+export default NotAb;
