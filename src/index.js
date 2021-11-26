9fcbb056aa4d6e80b2e51a4081f666c474e93e51
Lemit <Mihtrop2005@yandex.ru>
2021-11-26 16:35:18 +0300

    123321

--- src/index.js
+++ src/index.js
@@ -3,8 +3,7 @@ import ReactDOM from 'react-dom';
 import {IntlProvider} from "react-intl";
 import './index.css';
 import App from './components/app/App';
-import {Helmet} from "react-helmet";
-
+import {Helmet, HelmetProvider} from "react-helmet-async";
 import * as serviceWorker from './serviceWorker';
 import messages_ru from "./locales/ru.json";
 import messages_en from "./locales/en.json";
@@ -13,22 +12,20 @@ const messages = {
     'ru': messages_ru,
     'en': messages_en
 };
-
-var a=window.navigator?window.navigator.language||window.navigator.systemLanguage||window.navigator.g:"ru";a=a.substr(0,2).toLowerCase();alert(a);console.log(a);
-ReactDOM.render(<React.StrictMode>
+var a=window.navigator?window.navigator.language||window.navigator.systemLanguage||window.navigator.g:"ru";a=a.substr(0,2).toLowerCase();console.log(a);
+ReactDOM.render(
+    <HelmetProvider>
+<React.StrictMode>    
 <Helmet
 titleTemplate="lemito - %s"
 defaultTitle="lemito"
 >
 <html lang={a}  />
-
 </Helmet>
      <IntlProvider locale={a} messages={messages[a]}>
         <App/>
     </IntlProvider>
-</React.StrictMode>, document.getElementById('root'));
-
-
+</React.StrictMode> </HelmetProvider>
+, document.getElementById('root'));
 serviceWorker.unregister();
-
 /* Tip: For translation you can use BabelEdit */
